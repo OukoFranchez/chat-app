63f944396fe713cd284cd4962e4bdbfb0982c976
Stosh09 <stoshochieng@gmail.com>
2024-05-29 08:19:54 +0300

    Set up auth routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  console.log('signupUser');
+};
+
+export const login = (req, res) => {
+  console.log('LoginUser');
+};
+
+export const logout = (req, res) => {
+  console.log('LogoutUser');
+};
