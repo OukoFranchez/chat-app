8018c3a6daed02b7aa638dc62ce8cb6e30f009c1
Stosh09 <stoshochieng@gmail.com>
2024-05-29 14:10:17 +0300

    Create and integrate our db with app and create our sign up end point

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-const PORT= process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    // root route  http://localhost:5000/
-    res.send('Server is running!');
-});
+app.use(express.json()); //to parse incoming requests with JSON Payloads
 
 app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// app.get('/', (req, res) => {
+   // root route  http://localhost:5000/
+//   res.send('Server is running!');
+// });
+
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
