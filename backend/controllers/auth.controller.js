47ec22c458ae3a5f4c2a4e846b1b8c71117b3204
OukoFranchez <oukofranchez@gmail.com>
2024-05-30 00:40:22 +0300

    implement login controller with erro handling and token generation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-import User from '../models/user.models.js';
-import bcrypt from 'bcryptjs';
-import generateTokenAndSetCookie from '../utils/generateToken.js';
+import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -13,7 +13,7 @@ export const signup = async (req, res) => {
     const user = await User.findOne({ username });
 
     if (user) {
-      return res.status(400).json({ error: 'Username already exists' });
+      return res.status(400).json({ error: "Username already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -26,7 +26,7 @@ export const signup = async (req, res) => {
       fullName,
       username,
       password: hashedPassword,
-      profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
       gender,
     });
 
@@ -42,18 +42,41 @@ export const signup = async (req, res) => {
         gender: newUser.gender,
       });
     } else {
-      res.status(400).json({ error: 'Invalid user data' });
+      res.status(400).json({ error: "Invalid user data" });
     }
   } catch (error) {
-    console.log('Error in signup controller', error.message);
-    res.status(500).json({ error: 'Internal server error' });
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
-export const login = (req, res) => {
-  console.log('LoginUser');
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const logout = (req, res) => {
-  console.log('LogoutUser');
+  console.log("LogoutUser");
 };
