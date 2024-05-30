b69e30998a235f50f5cc7c717936bf8b53e95476
Stosh09 <stoshochieng@gmail.com>
2024-05-30 16:48:58 +0300

    Add login functionality, logout functionality, create a conversation and save the messages to the database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -10,14 +11,16 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); //to parse incoming requests with JSON Payloads
+app.use(cookieParser()); //to parse incoming requests with Cookies
 
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+
 
 // app.get('/', (req, res) => {
-   // root route  http://localhost:5000/
+// root route  http://localhost:5000/
 //   res.send('Server is running!');
 // });
-
 
 app.listen(PORT, () => {
   connectToMongoDB();
