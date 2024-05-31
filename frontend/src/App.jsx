d330d777bdbf84e1087cf9fabff743de1816f5be
Stosh09 <stoshochieng@gmail.com>
2024-05-31 22:49:41 +0300

    Add signup functionality and connect our backend to our frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import './App.css';
 import Chat from './pages/chat/Chat.jsx';
 import Login from './pages/login/Login.jsx';
 import SignUp from './pages/signup/SignUp.jsx';
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
         <Route path='/' element={<Chat />} />
         <Route path='/login' element={<Login />} />
-		<Route path='/signup' element={<SignUp />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
