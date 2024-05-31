ca8127a52e432138aa54d11d71f79bdd2145bd79
Stosh09 <stoshochieng@gmail.com>
2024-05-31 23:43:52 +0300

    Add AuthContext to our application to allow easy navigation to home page from Login and Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Navigate, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Chat from './pages/chat/Chat.jsx';
 import Login from './pages/login/Login.jsx';
 import SignUp from './pages/signup/SignUp.jsx';
 import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext';
 function App() {
+	const { authUser } = useAuthContext();
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
-        <Route path='/' element={<Chat />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path='/' element={authUser ? <Chat /> : <Navigate to={'/login'} /> } />
+        <Route
+          path='/login'
+          element={authUser ? <Navigate to='/' /> : <Login />}
+        />
+        <Route
+          path='/signup'
+          element={authUser ? <Navigate to='/' /> : <SignUp />}
+        />
       </Routes>
       <Toaster />
     </div>
