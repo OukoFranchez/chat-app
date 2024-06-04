589e64ce6edd9e86b8eee1bcadb900a4137e2104
Stosh09 <stoshochieng@gmail.com>
2024-06-04 23:58:37 +0300

    Get messages from db to the app to complete conversation

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,4 +1,5 @@
 import { useAuthContext } from "../../context/AuthContext"
+import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 const Message = ({message}) => {
@@ -6,25 +7,27 @@ const Message = ({message}) => {
 	const {authUser} = useAuthContext();
 	const {selectedConversation} = useConversation();
 	const fromMe = message.senderId === authUser._id;
+	const formattedDate = extractTime(message.createdAt);
 	const chatClassName = fromMe ? ' chat-end' : ' chat-start';
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
-	const bubbleBgColor = fromMe ? 'bg-blue-500' : 'bg-gray-300';
+	const bubbleBgColor = fromMe ? " bg-blue-500 " : '';
 
 
   return (
-	<div className={`chat ${chatClassName}`}>
-		<div className="chat-image avatar">
-			<div className="w-10 rounded-full">
-				<img
-					src={profilePic}
-					alt='Tailwind CSS chat bubble component'
-				/>
-			</div>
-		</div>
-		<div className={`chat-bubble text-white bg-blue-500${bubbleBgColor}`}>{message.message}</div>
-		<div className="chat-footer opacity-50 text-xs flex gap-1 items-center">20:07</div>
-	</div>
-  )
+    <div className={`chat ${chatClassName}`}>
+      <div className='chat-image avatar'>
+        <div className='w-10 rounded-full'>
+          <img src={profilePic} alt='Tailwind CSS chat bubble component' />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
+      </div>
+      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
+        {formattedDate}
+      </div>
+    </div>
+  );
 }
 
 export default Message
